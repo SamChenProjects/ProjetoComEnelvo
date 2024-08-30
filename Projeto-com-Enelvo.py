4c0484cb1ad4d5a9016b94c5a382d4f6fd10cb2e
Samuel Chen <analogpreto@gmail.com>
2024-08-30 19:09:22 -0300

    Add files via upload

---
+++ Projeto-com-Enelvo.py
@@ -0,0 +1,12 @@
+# Projeto simples com Enelvo
+from enelvo.normaliser import Normaliser
+
+norm = Normaliser(tokenizer='readable')
+
+normalizador = Normaliser(tokenizer='readable', capitalize_inis=True, 
+                          capitalize_pns=True, capitalize_acs=True, 
+                          sanitize=True)
+
+texto = 'eu gosto d aprinder sobre proceçamento d linguage naturl.'
+resultado = normalizador.normalise(texto)
+print(resultado)
